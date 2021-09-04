b3699b1c0527b050bf31db9857edf3492d536d95
kubotan <admin@gorogoro.space>
2021-09-04 15:05:05 +0900

    Bug fix.

--- src/main/java/space/gorogoro/firewall/Firewall.java
+++ src/main/java/space/gorogoro/firewall/Firewall.java
@@ -7,7 +7,16 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -22,10 +31,10 @@ import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Firewall extends JavaPlugin implements Listener{
-  FileConfiguration config;
-  List<String> blockNetsetIpv4List = new ArrayList<String>();
-  List<String> unblockIpAddrList = new ArrayList<String>();
-  List<String> unblockUuidList = new ArrayList<String>();
+  private Connection con;
+  private FileConfiguration config;
+  private ArrayList<ArrayList<Long>> blockNetsetIpv4List = new ArrayList<ArrayList<Long>>();
+  private List<String> unblockUuidList = new ArrayList<String>();
 
   @Override
   public boolean onCommand( CommandSender sender, Command commandInfo, String label, String[] args) {
@@ -57,6 +66,11 @@ public class Firewall extends JavaPlugin implements Listener{
 
   @Override
   public void onDisable(){
+    try {
+      con.close();
+    } catch (SQLException e) {
+      logStackTrace(e);
+    }
     getLogger().info("The Plugin Has Been Disabled!");
   }
 
@@ -66,19 +80,25 @@ public class Firewall extends JavaPlugin implements Listener{
       getLogger().info("The Plugin Has Been Enabled!");
       getServer().getPluginManager().registerEvents(this, this);
 
-      // If there is no setting file, it is created
-      if(!getDataFolder().exists()){
-        getDataFolder().mkdir();
-      }
       File configFile = new File(getDataFolder() + File.separator + "config.yml");
       if(!configFile.exists()){
         saveDefaultConfig();
       }
 
+      Class.forName("org.sqlite.JDBC");
+      con = DriverManager.getConnection("jdbc:sqlite:" + getDataFolder() + File.separator + "block.db");
+      con.setAutoCommit(false);
+      Statement stmt = con.createStatement();
+      stmt.setQueryTimeout(30);
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS block_list (id INTEGER PRIMARY KEY AUTOINCREMENT, cidr STRING NOT NULL, start LONG NOT NULL, end LONG NOT NULL);");
+      stmt.executeUpdate("CREATE UNIQUE INDEX IF NOT EXISTS block_list_cidr_uindex ON block_list (cidr, start, end);");
+      stmt.executeUpdate("CREATE INDEX IF NOT EXISTS block_list_start_end_index ON block_list (start, end);");
+      stmt.close();
+
       config = getConfig();
+      storeBlockNetsetList(con);
       loadBlockNetsetList();
-      unblockIpAddrList = config.getStringList("unblock-ip-addr-list");
-      unblockUuidList = config.getStringList("unblock-uuid-list");
+      unblockUuidList = config.getStringList("list-unblock-uuid");
 
     } catch (Exception e) {
       logStackTrace(e);
@@ -90,12 +110,10 @@ public class Firewall extends JavaPlugin implements Listener{
     try {
       String addr = event.getAddress().getHostAddress().toString();
 
-      if(unblockIpAddrList.contains(addr) == false
-        && unblockUuidList.contains(event.getUniqueId().toString()) == false) {
-        for(String cidr:blockNetsetIpv4List) {
-          if(cidrInIpv4(addr, cidr)) {
+      if(unblockUuidList.contains(event.getUniqueId().toString()) == false) {
+        for(ArrayList<Long> range:blockNetsetIpv4List) {
+          if(range.get(0) <= ipv4ToLong(addr) && ipv4ToLong(addr) <= range.get(1)) {
             event.disallow(Result.KICK_OTHER, config.getString("message-kick"));
-            return;
           }
         }
       }
@@ -104,22 +122,73 @@ public class Firewall extends JavaPlugin implements Listener{
     }
   }
 
-  private void loadBlockNetsetList() {
+  private void storeBlockNetsetList(Connection con) throws SQLException {
     try {
-      blockNetsetIpv4List = null;
-      for(String fileName : config.getStringList("block-netset-file-list")){
+      File dest = new File(getDataFolder() + File.separator + "stored" + File.separator);
+      if(!dest.exists()) {
+        dest.mkdir();
+      }
+      for(String fileName : config.getStringList("list-block-netset-file")){
         File f = new File(getDataFolder() + File.separator + fileName);
         if(!f.exists()) {
           continue;
         }
+        getLogger().info("Found. path=" + f.getPath());
 
         List<String> lines = Files.readAllLines(f.toPath());
+        HashMap<String,PreparedStatement> prepStmt = new HashMap<>();
+        prepStmt.put("select",con.prepareStatement("SELECT 1 FROM block_list WHERE cidr = ?;"));
+        prepStmt.put("insert",con.prepareStatement("INSERT INTO block_list(cidr, start, end) VALUES (?, ?, ?);"));
+        ResultSet rs;
+        String[] range;
+        int rows = 0;
         for(String line: lines) {
-          if(isIpv4(line) && !blockNetsetIpv4List.contains(line)) {
-            blockNetsetIpv4List.add(line);
+          if(!isIpv4(line)) {
+            continue;
           }
+
+          prepStmt.get("select").setString(1, line);
+          rs = prepStmt.get("select").executeQuery();
+          if(rs.next()){
+            rs.close();
+            continue;
+          }
+          rs.close();
+
+          prepStmt.get("insert").setString(1, line);
+          range = cidrToIpv4(line);
+          prepStmt.get("insert").setLong(2, ipv4ToLong(range[0]));
+          prepStmt.get("insert").setLong(3, ipv4ToLong(range[1]));
+          prepStmt.get("insert").addBatch();
+          rows++;
         }
+        prepStmt.get("insert").executeBatch();
+        con.commit();
+        for(PreparedStatement p:prepStmt.values()) {
+          p.close();
+        }
+
+        Files.move(Paths.get(f.getPath()), Paths.get(dest.getPath() + File.separator + fileName));
+        getLogger().info("Stored. rows=" + rows + " path=" + f.getPath());
       }
+      con.commit();
+    } catch (Exception e) {
+      logStackTrace(e);
+      con.rollback();
+    }
+  }
+
+  private void loadBlockNetsetList() {
+    try {
+      blockNetsetIpv4List = new ArrayList<ArrayList<Long>>();
+      HashMap<String,PreparedStatement> prepStmt = new HashMap<>();
+      prepStmt.put("select",con.prepareStatement("SELECT start, end FROM block_list;"));
+      ResultSet rs;
+      rs = prepStmt.get("select").executeQuery();
+      while(rs.next()){
+        blockNetsetIpv4List.add(new ArrayList<Long>(Arrays.asList(rs.getLong(1), rs.getLong(2))));
+      }
+      rs.close();
     } catch (Exception e) {
       logStackTrace(e);
     }
@@ -127,19 +196,15 @@ public class Firewall extends JavaPlugin implements Listener{
 
   private static boolean isIpv4(String str) {
     try {
-      if (InetAddress.getByName(str) instanceof Inet4Address) {
+      if(str.length() < 7 || str.length() > 18) {
+        return false;
+      }
+      String[] arrIp = str.split("/");
+      if (InetAddress.getByName(arrIp[0]) instanceof Inet4Address) {
         return true;
       }
     } catch (UnknownHostException e) {
       return false;
-    }
-    return false;
-  }
-
-  private static boolean cidrInIpv4(String addr, String cidr) {
-    String[] range = cidrToIpv4(cidr);
-    if(ipv4ToLong(range[0]) <= ipv4ToLong(addr) && ipv4ToLong(addr) <= ipv4ToLong(range[1])) {
-      return true;
     }
     return false;
   }
